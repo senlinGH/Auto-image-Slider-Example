75cf47e49a7a7e2c56ad0b637599170d6bfbd565
senlin32 <senlin32@hotmail.com>
2021-03-06 14:34:10 +0800

    Initial Commit

---
+++ Auto image Slider Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto image Slider Example
+//
+//  Created by Lin Yi Sen on 2021/3/6.
+//  Copyright © 2021 Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
