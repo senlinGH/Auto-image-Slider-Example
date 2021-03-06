39465fb99657c790861ce5a93a7e61d605d95b3a
senlin32 <senlin32@hotmail.com>
2021-03-06 20:50:43 +0800

    完成廣告滑頁功能，但PageControl未完成

---
+++ Auto image Slider Example/SliderCell.swift
@@ -0,0 +1,15 @@
+//
+//  SliderCell.swift
+//  Auto image Slider Example
+//
+//  Created by Lin Yi Sen on 2021/3/6.
+//  Copyright © 2021 Swift. All rights reserved.
+//
+
+import UIKit
+
+class SliderCell: UICollectionViewCell {
+    
+    @IBOutlet weak var sliderImage: UIImageView!
+    
+}
--- Auto image Slider Example/ViewController.swift
+++ Auto image Slider Example/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sliderCollectionView: UICollectionView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    let airJordanArr = ["aj1", "aj2", "aj3", "aj4", "aj5"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        sliderCollectionView.delegate = self
+        sliderCollectionView.dataSource = self
     }
 
 
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        airJordanArr.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = sliderCollectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! SliderCell
+        cell.sliderImage.image = UIImage(named: airJordanArr[indexPath.row])
+        
+        return cell
+    }
+    
+    
+    
 }
 
